a5787e3e93db84187c777d194b4afbc3cc336184
Mohd Ajlal <ajlal00786@gmail.com>
2024-07-12 05:59:45 +0530

    MeetingModal

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -10,6 +10,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
 
 
 
@@ -31,9 +34,44 @@ const MeetingModel = ({isOpen, onClose, title, className, children, handleClick,
     <Dialog open={isOpen} onOpenChange={onClose}>
       {/* <DialogTrigger></DialogTrigger> */}
       <DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white'>
-        <div className=''>
+        {/* <div className='flex flex-col gap-6'>
+          {image && (
+            <div className='flex justify-center'>
+              <Image
+              src={image}
+              alt='image'
+              width={72}
+              height={72}
+              />
+              <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+            {title}
+          </h1>
+            </div>
+          )}
 
-        </div>
+        </div> */}
+        <DialogHeader className='flex flex-col gap-6'>
+      <DialogTitle className={cn("text-3xl font-bold leading-[42px]", className)}>{title}</DialogTitle>
+      {children}
+      <Button
+      className={
+        "bg-purple-1 focus-visible:ring-0 focus-visible:ring-offset-0"
+      }
+      onClick={handleClick}>
+        {buttonIcon && (
+              <Image
+                src={buttonIcon}
+                alt="button icon"
+                width={13}
+                height={13}
+              />
+            )}{" "}
+            &nbsp;
+            {buttonText || "Schedule Meeting"}
+
+      </Button>
+    </DialogHeader>
+
       </DialogContent>
 </Dialog>
 
